41fa211101460f9b42bb8032b6e01b6e0547d485
dailystudio <dailystudio2010@gmail.com>
2014-02-27 12:16:49 +0800

    [*][DEV]: add getTopActivity() and isActivityLaunched() in ActivityLauncher

--- DailyAppLib/src/com/dailystudio/app/utils/ActivityLauncher.java
+++ DailyAppLib/src/com/dailystudio/app/utils/ActivityLauncher.java
@@ -1,7 +1,12 @@
 package com.dailystudio.app.utils;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningTaskInfo;
 import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -70,5 +75,38 @@ public class ActivityLauncher {
 		launchActivityForResult(activity, intent, requestCode, sOnExceptionHandler);
 	}
 
+	public static boolean isActivityLaunched(Context context, Context appContext, Class<?> cls) {
+		return isActivityLaunched(context, new ComponentName(appContext, cls));
+	}
+	
+	public static boolean isActivityLaunched(Context context, String pkg, String cls) {
+		return isActivityLaunched(context, new ComponentName(pkg, cls));
+	}
+	
+	public static boolean isActivityLaunched(Context context, ComponentName activityComp) {
+		if (context == null || activityComp == null) {
+			return false;
+		}
+
+		final ComponentName topActivity = getTopActivity(context);
+		
+		return (activityComp.compareTo(topActivity) == 0);
+	}
+	
+	public static ComponentName getTopActivity(Context context) {
+		if (context == null) {
+			return null;
+		}
+		
+		ActivityManager actmgr = 
+				(ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		
+		List<RunningTaskInfo> taskInfos = actmgr.getRunningTasks(1);
+		if (taskInfos == null || taskInfos.size() <= 0) {
+			return null;
+		}
+		
+		return taskInfos.get(0).topActivity;
+	}
 	
 }
