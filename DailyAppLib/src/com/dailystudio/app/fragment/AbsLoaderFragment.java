4b2d576162f6ee2761afd9f43ecf721c992e7d3a
dailystudio <dailystudio2010@gmail.com>
2014-05-27 14:17:34 +0800

    [*][DEV]: add base case to provide bindIntent for fragment;

--- DailyAppLib/src/com/dailystudio/app/fragment/AbsLoaderFragment.java
+++ DailyAppLib/src/com/dailystudio/app/fragment/AbsLoaderFragment.java
@@ -2,27 +2,21 @@ package com.dailystudio.app.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 
-public abstract class AbsLoaderFragment<T> extends Fragment implements LoaderCallbacks<T> {
+public abstract class AbsLoaderFragment<T> extends BaseIntentFragment implements LoaderCallbacks<T> {
     
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		
-		bindIntent(getActivity().getIntent());
-		
 		getLoaderManager().initLoader(getLoaderId(), createLoaderArguments(), this);
 	}
 	
 	public void onNewIntent(Intent intent) {
-		bindIntent(intent);
+		super.onNewIntent(intent);
 		
 		restartLoader();
-	}
-
-	protected void bindIntent(Intent intent) {
 	}
 	
 	public void restartLoader() {
