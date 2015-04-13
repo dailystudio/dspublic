4c2225f1d64d912c62d9532d5b5c18adc9c15a27
dailystudio <dailystudio2010@gmail.com>
2015-04-13 15:37:42 +0800

    [*][REL]: release v1.0.4 [*][DEV]: add proguard support

--- DailyAppLib/src/com/dailystudio/app/async/AsyncChecker.java
+++ DailyAppLib/src/com/dailystudio/app/async/AsyncChecker.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 
-import com.dailystudio.app.async.AsyncTasksQueueExecutor;
 import com.dailystudio.datetime.CalendarUtils;
 import com.dailystudio.development.Logger;
 import com.dailystudio.utils.ClassNameUtils;
